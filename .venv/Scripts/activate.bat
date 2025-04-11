ccbca95167f55b352b74862d1bcf56bde34853b6
Manas Pradhan <manasranjanpradhan2004@gmail.com>
2025-04-11 08:51:37 +0530

    Fronted enhance

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=D:\Devloped projects\ProjectWithSibo\BridgeWork\.venv"
+set "VIRTUAL_ENV=D:\Devloped projects\ProjectWithSibo\LinkedMatrix\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
